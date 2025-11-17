5c41b17645b83c18dc6f708b97831c98374d4d17
bmenc <bmenchaca.cca@gmail.com>
2025-11-17 16:50:37 -0600

    113. Creando un Hook para la orden

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -7,7 +7,7 @@ type MenuItemProps = {
 export default function MenuItem({item}: MenuItemProps) {
   return (
     <button
-      className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+      className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between rounded-md"
     >
       <p>{item.name}</p>
       <p className="font-black">${item.price}</p>

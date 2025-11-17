4f28abb14fcf3f195cba0f139a3f71a4615a8edd
bmenc <bmenchaca.cca@gmail.com>
2025-11-16 19:41:42 -0600

    112. Listando los productos del menú

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MenuItem from "./components/MenuItem";
 import { menuItems } from "./data/db";
 
 function App() {
@@ -9,11 +10,19 @@ function App() {
         <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2>Menú</h2>
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">            
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+              />
+            ))}
+          </div>
         </div>
         <div>
-          col2
+          Consumo
         </div>
       </main>
     </>

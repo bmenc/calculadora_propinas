aadaf0908f2982a6aac6d91410fc13a1c7fa73e6
bmenc <bmenchaca.cca@gmail.com>
2025-11-17 18:06:27 -0600

    114. Creando la función para Añadir articulos a la orden

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import MenuItem from "./components/MenuItem";
 import { menuItems } from "./data/db";
+import useOrder from "./hooks/useOrder";
 
 function App() {
   console.log(menuItems);
+  const { addItem } = useOrder();
 
   return (
     <>
@@ -12,11 +14,12 @@ function App() {
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
         <div className="p-5">
           <h2 className="text-4xl font-black">Menú</h2>
-          <div className="space-y-3 mt-10">            
+          <div className="space-y-3 mt-10">
             {menuItems.map(item => (
               <MenuItem
                 key={item.id}
                 item={item}
+                addItem={addItem}
               />
             ))}
           </div>

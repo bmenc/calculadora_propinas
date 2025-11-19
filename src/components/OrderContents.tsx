f3fe9b1d7ff9e6b0297664793455d4c5dafd4f87
bmenc <bmenchaca.cca@gmail.com>
2025-11-18 19:38:56 -0600

    120. Eliminando articulos de la orden

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,11 +1,12 @@
 import { formatCurrency } from "../helpers"
-import type { OrderItem } from "../types"
+import type { MenuItem, OrderItem } from "../types"
 
 type OrderContentsProps = {
-    order: OrderItem[]
+    order: OrderItem[],
+    removeItem: (id: MenuItem['id']) => void
 }
 
-export default function OrderContents({ order }: OrderContentsProps) {
+export default function OrderContents({ order, removeItem }: OrderContentsProps) {
     return (
         <div>
             <h2 className="font-black text-4xl">Consumo</h2>
@@ -27,7 +28,9 @@ export default function OrderContents({ order }: OrderContentsProps) {
                                 </p>
                             </div>
 
-                            <button className="bg-red-600 h-8 w-8 rounded-full text-white font-bold">X</button>
+                            <button
+                                className="bg-red-600 h-8 w-8 rounded-full text-white font-bold"
+                                onClick={() => removeItem(item.id)}>X</button>
                         </div>
                     )))
                 }

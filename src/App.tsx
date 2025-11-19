055a5b91a001c46d11a71d3b076b7a43cfa18082
bmenc <bmenchaca.cca@gmail.com>
2025-11-18 19:58:57 -0600

    122. Calculando el Consumo y más TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,9 @@ function App() {
             order={order}
             removeItem={removeItem}
           />
-          <OrderTotals/>
+          <OrderTotals
+            order={order}
+          />
         </div>
       </main>
     </>
--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,15 +1,25 @@
-export default function OrderTotals() {
+import { useMemo } from "react"
+import type { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
+
+type OrderTotalsProps = {
+    order: OrderItem[]
+}
+
+export default function OrderTotals({order}: OrderTotalsProps) {
+    const subtotalAmout = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+
     return (
         <>
             <div className="space-y-3">
                 <h2 className="font-black text-2xl">Totales y Propina:</h2>
-                <p>Subtotal a pagar:{''}
+                <p>Subtotal a pagar: {''}
+                    <span className="font-bold">{formatCurrency(subtotalAmout)}</span>
+                </p>
+                <p>Propina: {''}
                     <span className="font-bold">0</span>
                 </p>
-                <p>Propina:{''}
-                    <span className="font-bold">0</span>
-                </p>
-                <p>Total a Pagar:{''}
+                <p>Total a Pagar: {''}
                     <span className="font-bold">0</span>
                 </p>
             </div>

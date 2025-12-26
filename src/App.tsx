fdefb7161afc729a414e1ef2b00d7dd182b0be40
bmenc <bmenchaca.cca@gmail.com>
2025-12-25 19:45:02 -0600

    184. Mostrando los contenidos de la orden

--- src/App.tsx
+++ src/App.tsx
@@ -30,10 +30,10 @@ function App() {
           </div>
         </div>
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          {order.length ? (
+          {state.order.length ? (
             <>
               <OrderContents
-                order={order}
+                order={state.order}
                 removeItem={removeItem}
               />
               <TipPercentageForm
